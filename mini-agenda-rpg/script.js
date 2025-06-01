04bbe31525ee960ea191d8260862d086ef37ba03
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-06-01 16:29:16 -0300

    Ajustar estrutura do HTML e CSS para a seção de tarefas

--- mini-agenda-rpg/script.js
+++ mini-agenda-rpg/script.js
@@ -1,3 +1,13 @@
 const campo = document.getElementById('campo-tarefa');
 const botao = document.getElementById('adicionar');
 const lista = document.getElementById('lista-tarefas');
+
+botao.addEventListener('click', function() {
+  const texto = campo.value;
+  if (texto) {
+    const li = document.createElement('li');
+    li.textContent = texto;
+    lista.appendChild(li);
+    campo.value = '';
+  }
+});
